f0ab57363404004d293e1fb02f301cf91cd131d3
Lee Chuan Xin <chuanxin@gametize.com>
2021-08-08 07:48:44 +0800

    feat: path params for fruits.

--- index.js
+++ index.js
@@ -10,12 +10,55 @@ app.get('/fruit', (request, response) => {
   // Obtain data to inject into EJS template
   const data = {
     fruit: {
-      species: 'M. domestica',
+      family: 'Musaceae',
       name: 'banana',
     },
   };
   // Return HTML to client, merging "index" template with supplied data.
-  response.render('fruit', data);
+  response.render('fruit', data.fruit);
+});
+
+app.get('/fruits/:name', (request, response) => {
+  // Obtain data to inject into EJS template
+  const data = {
+    fruits: [
+      {
+        family: 'Musaceae',
+        name: 'banana',
+      },
+      {
+        family: 'Rosaceae',
+        name: 'apple',
+      },
+      {
+        family: 'Rosaceae',
+        name: 'pear',
+      },
+      {
+        family: 'Malvaceae',
+        name: 'durian',
+      },
+      {
+        family: 'Myrtaceae',
+        name: 'guava',
+      },
+      {
+        family: 'Rutaceae',
+        name: 'lemon',
+      },
+    ],
+  };
+  const idx = data.fruits.findIndex(
+    (fruit) => fruit.name.toLowerCase() === request.params.name,
+  );
+
+  if (idx > -1) {
+    const fruit = data.fruits[idx];
+    // Return HTML to client, merging "index" template with supplied data.
+    response.render('fruit', fruit);
+  } else {
+    response.status(404).send('Sorry, fruit not found.');
+  }
 });
 
 app.listen(PORT);
